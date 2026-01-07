9ffbf03db54b88773ee9addcc72e47f14b43b1fc
unknown <newli5737@gmail.com>
2026-01-07 15:19:25 +0700

    updaet

--- reco_face_api/app/main.py
+++ reco_face_api/app/main.py
@@ -105,4 +105,4 @@ async def compare_faces(request: CompareFacesRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    uvicorn.run(app, host="0.0.0.0", port=8003)
